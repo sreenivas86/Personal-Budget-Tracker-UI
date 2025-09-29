dbd16704655ffdb0ffbbd3a133c7d43ed7346783
Sreenivas Reddy Daram <147172875+sreenivas86@users.noreply.github.com>
2025-09-29 16:35:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,8 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/myapp/">   {/* ← add this */}
+       
       <App />
-    </BrowserRouter>
+   
   </StrictMode>
 );
