4478d3575bfc8a601fbee610f6f61a4986e534e6
Sreenivas Reddy Daram <147172875+sreenivas86@users.noreply.github.com>
2025-09-29 12:34:12 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-# List of runtime environment variables
+# List of runtime environment variables to inject
 RUNTIME_VARS="VITE_API_URL VITE_OTHER_VAR"
+
+# Path to the config file
+CONFIG_FILE="/usr/share/nginx/html/config.js"
 
 # Replace placeholders in config.js with actual env values
 for var in $RUNTIME_VARS; do
-  if [ -n "${!var}" ]; then
-    echo "Setting $var to ${!var}"
-    sed -i "s|__${var}__|${!var}|g" /usr/share/nginx/html/config.js
+  value=$(eval echo \$$var)
+  if [ -n "$value" ]; then
+    echo "Injecting $var=$value into config.js"
+    sed -i "s|__${var}__|$value|g" "$CONFIG_FILE"
+  else
+    echo "Warning: $var is not set, skipping substitution"
   fi
 done
 
-# Start nginx
-nginx -g "daemon off;"
+# Start nginx in foreground
+exec nginx -g "daemon off;"
